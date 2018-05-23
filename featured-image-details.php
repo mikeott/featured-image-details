cc633f4951c8ccc283743c8a6e5a73795244d3df
Mike Ott <hello@michaelott.id.au>
2018-05-23 15:55:08 +0800

    First version ready.

--- featured-image-details.php
+++ featured-image-details.php
@@ -5,7 +5,7 @@ Plugin URI: https://michaelott.id.au
 Description: Qickly view details of the featured image.
 Author: Michael Ott
 Version: 1.0
-Text Domain: featured-image-modal
+Text Domain: fid
 Domain Path: /languages/
 */
 
@@ -15,27 +15,27 @@ function load_fid_textdomain() {
 }
 add_action( 'plugins_loaded', 'load_fid_textdomain' );
 
-// Add popup JS into admin head
+// Add popup CSS into admin head
 function fim_admin() { ?>
     <style>
     .fid-window {
         display: none; 
         position: fixed; 
-        width: calc(100% - 100px); 
-        height: calc(100% - 100px); 
-        top: 50px; 
-        left: 50px;
-        z-index: 99999;
+        width: 100%; 
+        height: 100%; 
+        top: 0; 
+        left: 0;
+        z-index: 999991;
     }
     .fid-mask {
         display: none; 
-        background: rgba(0, 0, 0, 0.60);
+        background: rgba(0, 0, 0, 0.80);
         position: fixed; 
         width: 100%; 
         height: 100%; 
         top: 0; 
         left: 0;
-        z-index: 99998;
+        z-index: 99999;
     }
     </style>
 <?php }
@@ -46,10 +46,10 @@ function add_featured_image_display_settings( $content, $post_id ) {
     $button_label           = esc_html__( 'Featured Image Details', 'fid' );
     $post_thumbnail_id      = get_post_thumbnail_id( $post_id );
     $details_path           = get_admin_url() . 'post.php?post=' . $post_thumbnail_id . '&action=edit';
-    //$fid_image_preview      = get_the_post_thumbnail_url(get_the_ID(),'full');
-    $fid_image_preview      = plugins_url( 'preview.php', __FILE__ );
+    $fid_image_path         = get_the_post_thumbnail_url(get_the_ID(),'full');
+    $fid_image_preview      = plugins_url( 'preview.php', __FILE__ ) . '?path=' . $fid_image_path . '&postID=' . $post_id;
     $fid_button             = sprintf('<p><a class="button fid-button">%1$s</a></p>',$button_label);
-    $fid_script             = '<script>jQuery(".fid-button").click(function() { jQuery(".fid-window").load("' . $fid_image_preview . '"); jQuery(".fid-mask, .fid-window").fadeIn(); });</script>';
+    $fid_script             = '<script>jQuery(".fid-button").click(function() { jQuery(document).ready(function(){jQuery(".fid-window").load("'. $fid_image_preview . '");}); jQuery(".fid-mask, .fid-window").fadeIn(); });</script>';
     $fid_window             = '<div class="fid-window"></div>';
     $fid_mask               = '<div class="fid-mask"></div>';
 
--- preview.php
+++ preview.php
@@ -1,2 +1,197 @@
-<?php // Image details
-echo '<h1 style="color:#fff; font-size: 100px;">xxxxxxxxxxxxxxxxxxxxxxxxxxxxx</h1>';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Preview</title>
+    <style>
+        *, {
+            margin:0;
+            padding: 0;
+        }
+        body. html {
+            height: 100%;
+            position: relative;
+        }
+        .fid-align {
+            display: block;
+            width: 70%;
+            height: 100%;
+            position: fixed;
+            top: 50%;
+            transform: translateY(-50%);
+            left: 15%;
+            
+        }
+        .fid-preview {
+            display: block;
+            max-width: 100%;
+            max-height: 100%;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-right: -50%;
+            transform: translate(-50%, -50%);
+            box-sizing: border-box;
+            box-shadow: 0 0 100px rgba(0, 0, 0, 0.3);
+        }
+        .fid-specs {
+            display: block;
+            padding: 25px;
+            box-sizing: border-box;
+            background: rgba(0, 0, 0, 0.80);
+            width: 200px;
+            //height: 100%;
+            position: absolute;
+            top: 0;
+            right: 0;
+            text-align: left;
+        }
+        .fid-preview p {
+            color: #fff;
+            margin: 15px 0;
+            font-family: monospace;
+        }
+
+        .fid-preview p:first-child {
+            margin: 0 0 15px 0;
+        }
+        .fid-preview p:last-child {
+            margin: 15px 0 0 0;
+        }
+
+        .fid-preview p span {
+            display: block;
+            line-height: 1em;
+            overflow-wrap: break-word;
+        }
+        .fid-preview p small {
+            font-size: .8em;
+            opacity: .7;
+        }
+        .fid-preview img {
+            cursor: crosshair;
+        }
+        .close {
+            color: #fff;
+            width: 40px;
+            height: 40px;
+            font-size: 2.4em;
+            position: absolute;
+            top: -40px;
+            right: 0;
+            cursor: pointer;
+        }
+        hr {
+            height: 0;
+            border: none;
+            border-top: solid 1px #666;
+            margin: 20px 0;
+        }
+        .clear {
+            display:block;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+<?php
+    require_once('../../../wp-load.php');
+    $path               = $_GET['path'];
+    $post_ID            = $_GET['postID'];
+    $image_data         = wp_get_attachment_metadata( get_post_thumbnail_id( $post_ID ) );
+   
+    $width              = $image_data['width'];
+    $height             = $image_data['height'];
+    $file_path          = $image_data['file'];
+
+    $camera             = $image_data['image_meta']['camera'];
+    $focal_length       = $image_data['image_meta']['focal_length'];
+    $iso                = $image_data['image_meta']['iso'];
+    $shutter_speed      = $image_data['image_meta']['shutter_speed'];
+    $aperture           = $image_data['image_meta']['aperture'];
+    $created_timestamp  = $image_data['image_meta']['created_timestamp'];
+    $image_alt          = get_post_meta( get_post_thumbnail_id( $post_ID ), '_wp_attachment_image_alt', true);
+    $image_caption      = get_post_meta( get_post_thumbnail_id( $post_ID ), '_wp_attachment_image_caption', true);
+
+?>
+<div class="fid-align">
+    <div class="fid-preview">
+        <span class="dashicons dashicons-no close"></span>
+        <img src="<?php echo $path; ?>" />
+        <div class="fid-specs">
+            <p>
+                <small><?php _e( 'Width', 'fid' ); ?></small>
+                <span><?php echo $width; ?>px</span>
+            </p>
+            <p>
+                <small><?php _e( 'Height', 'fid' ); ?></small>
+                <span><?php echo $height; ?>px</span>
+            </p>
+            <p>
+                <small><?php _e( 'File Path', 'fid' ); ?></small>
+                <span><?php echo $file_path; ?></span>
+            </p>
+
+            <?php if($image_alt) { ?>
+            <p>
+                <small><?php _e( 'Alt', 'fid' ); ?></small>
+                <span><?php echo $image_alt; ?></span>
+            </p>
+            <?php } ?>
+
+            <?php if($camera) { ?>
+            <p>
+                <small><?php _e( 'Camera', 'fid' ); ?></small>
+                <span><?php echo $camera; ?></span>
+            </p>
+            <?php } ?>
+
+            <?php if($focal_length) { ?>
+            <p>
+                <small><?php _e( 'Focal length', 'fid' ); ?></small>
+                <span><?php echo $focal_length; ?></span>
+            </p>
+            <?php } ?>
+
+            <?php if($iso) { ?>
+            <p>
+                <small><?php _e( 'ISO', 'fid' ); ?></small>
+                <span><?php echo $iso; ?></span>
+            </p>
+            <?php } ?>
+
+            <?php if($shutter_speed) { ?>
+            <p>
+                <small><?php _e( 'Shutter Speed', 'fid' ); ?></small>
+                <span><?php echo $shutter_speed; ?></span>
+            </p>
+            <?php } ?>
+            
+            <?php if($aperture) { ?>
+            <p>
+                <small><?php _e( 'Aperture', 'fid' ); ?></small>
+                <span><?php echo $aperture; ?></span>
+            </p>
+            <?php } ?>
+
+            <?php if($created_timestamp) { ?>
+            <p>
+                <small><?php _e( 'Timestamp', 'fid' ); ?></small>
+                <span><?php echo $created_timestamp; ?></span>
+            </p>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+<script>
+    jQuery('.close').click(function() {
+        jQuery('.fid-mask, .fid-window').fadeOut();
+    });
+    jQuery('img').click(function() {
+        jQuery('.fid-specs').fadeToggle();
+    });
+</script>
+</body>
+</html>
